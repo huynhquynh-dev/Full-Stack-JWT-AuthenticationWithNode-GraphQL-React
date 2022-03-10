4ea67a3655142769e44db5a924bcc33d545f54e2
huynhquynh-dev <huynhquynh-dev>
2022-03-10 20:30:08 +0700

    -Add connect Postgres

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,21 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
-import 'reflect-metadata';
-
-import { createConnection } from 'typeorm';
-import { User } from './entities/User';
-
+require("reflect-metadata");
+const typeorm_1 = require("typeorm");
+const User_1 = require("./entities/User");
 const main = async () => {
-    await createConnection({
+    await (0, typeorm_1.createConnection)({
         type: 'postgres',
         database: 'full-stack-jwt-auth-henry',
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         logging: true,
         synchronize: true,
-        entities: [User],
+        entities: [User_1.User],
     });
 };
-
 main().catch((err) => {
     console.log(err);
 });
+//# sourceMappingURL=index.js.map
